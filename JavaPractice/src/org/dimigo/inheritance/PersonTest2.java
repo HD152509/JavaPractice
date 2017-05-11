4017d8fbd362db282ccad4f9a77e330ae17a6503
duddn <duddn@DESKTOP-M8DSSAL>
2017-05-11 09:11:40 +0900

    실습과제 10

---
+++ JavaPractice/src/org/dimigo/inheritance/PersonTest2.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+package org.dimigo.inheritance;
+
+/**
+ * <pre>
+ * org.dimigo.inheritance
+ *   |_PersonTest
+ *
+ * 1. 개요 :
+ * 2. 작성일 : 2017. 5. 8.
+ * </pre>
+ *
+ * @author : duddn
+ * @version : 1.0
+ */
+public class PersonTest2 {
+	
+	public static void main(String[] args)
+	{
+		Person[] ppp = {
+				new Person("Tom"),
+				new Korean("홍길동"),
+				new Japanese("다나카"),
+				new Chinese("왕밍")
+		};
+		
+		for(Person aaa : ppp){
+			greeting(aaa);
+		}
+	}
+	
+	private static void greeting(Person p){
+		
+		System.out.println(p.toString());
+		p.sayHello();
+		p.sayBye();
+		System.out.print('\n');
+	}
+	
+	
+
+}
