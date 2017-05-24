952249b22862c92add53b1a4ebef68329a757177
duddn <duddn@DESKTOP-M8DSSAL>
2017-05-24 15:35:42 +0900

    실습과제 13

---
+++ JavaPractice/src/interfaces/DBTest.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+package interfaces;
+
+/**
+ * <pre>
+ * interfaces
+ *   |_DBTest
+ *
+ * 1. 개요 :
+ * 2. 작성일 : 2017. 5. 24.
+ * </pre>
+ *
+ * @author : duddn
+ * @version : 1.0
+ */
+public class DBTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		IDBManager idb = IDBManager.getDBObject("SYBASE");
+		System.out.println("< 변경 전 >");
+		crud(idb);
+		IDBManager idb2 = IDBManager.getDBObject("ORACLE");
+		System.out.println("< 변경 후 >");
+		crud(idb2);		
+
+	}
+	
+	private static void crud(IDBManager db){
+		db.insert();
+		db.search();
+		db.update();
+		db.delete();
+	}
+
+}
