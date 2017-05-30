6ab321079a9a4aacfe592edaf678d25f529e710d
duddn <duddn@DESKTOP-M8DSSAL>
2017-05-30 12:49:26 +0900

    실습과제 14

---
+++ JavaPractice/src/org/dimigo/exception/Movie.java
@@ -0,0 +1,45 @@
+/**
+ * 
+ */
+package org.dimigo.exception;
+
+/**
+ * <pre>
+ * org.dimigo.exception
+ *   |_Movie
+ *
+ * 1. 개요 :
+ * 2. 작성일 : 2017. 5. 29.
+ * </pre>
+ *
+ * @author : duddn
+ * @version : 1.0
+ */
+public class Movie {
+	
+	private String title;
+	private int limitAge;
+	
+	public Movie(String title, int limitAge){
+		this.title = title;
+		
+		this.limitAge = limitAge;
+	}
+	
+	public String getTitle(){
+		return title;
+	}
+	
+	public int getLimitAge(){
+		return limitAge;
+		
+	}
+	
+	public void buyTicket(int age) throws Exception{
+		if(age < getLimitAge()){
+			throw new Exception(this.getTitle() + "은/는" + this.getLimitAge() + "세 이상 관람가입니다.");
+		}
+		else System.out.println(this.getTitle() + " 즐감하세요.");
+	}
+
+}
---
+++ JavaPractice/src/org/dimigo/exception/MovieTest.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+package org.dimigo.exception;
+
+/**
+ * <pre>
+ * org.dimigo.exception
+ *   |_MovieTest
+ *
+ * 1. 개요 :
+ * 2. 작성일 : 2017. 5. 29.
+ * </pre>
+ *
+ * @author : duddn
+ * @version : 1.0
+ */
+public class MovieTest {
+
+	/**
+	 * @param args
+	 * @throws AgeCheckException
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Movie[] movies = { new Movie("에이리언:커버넌트", 15), new Movie("가디언즈 오브 갤럭시", 12) };
+
+		int age = 13;
+
+		for (Movie movie : movies) {
+			try {
+				movie.buyTicket(age);
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+			}
+		}
+
+	}
+
+}
